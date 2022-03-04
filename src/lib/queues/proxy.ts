d94d590687ba3cea106534e101bb4e8746d090ec
zero-0002 <zero.dev0002@gmail.com>
2022-03-04 14:20:20 +0100

    fix: do not error if proxy is not running

---
+++ src/lib/queues/proxy.ts
@@ -0,0 +1,11 @@
+import { ErrorHandler } from '$lib/database';
+import { configureHAProxy } from '$lib/haproxy/configuration';
+
+export default async function () {
+	try {
+		return await configureHAProxy();
+	} catch (error) {
+		console.log(error.response?.body || error);
+		return ErrorHandler(error.response?.body || error);
+	}
+}
