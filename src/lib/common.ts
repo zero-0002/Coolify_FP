2df484b21aa2e1d10f5c3b00b546b6a26d5b38e0
zero-0002 <zero.dev0002@gmail.com>
2022-03-11 21:18:12 +0100

    feat: Webhooks inititate all applications with the correct branch

---
+++ src/lib/common.ts
@@ -0,0 +1,153 @@
+import child from 'child_process';
+import util from 'util';
+import { dev } from '$app/env';
+import * as Sentry from '@sentry/node';
+import { uniqueNamesGenerator, Config, adjectives, colors, animals } from 'unique-names-generator';
+
+import * as db from '$lib/database';
+import { buildLogQueue } from './queues';
+
+import { version as currentVersion } from '../../package.json';
+import dayjs from 'dayjs';
+import Cookie from 'cookie';
+import os from 'os';
+import cuid from 'cuid';
+
+try {
+	if (!dev) {
+		Sentry.init({
+			dsn: process.env['COOLIFY_SENTRY_DSN'],
+			tracesSampleRate: 0,
+			environment: 'production',
+			debug: true,
+			release: currentVersion,
+			initialScope: {
+				tags: {
+					appId: process.env['COOLIFY_APP_ID'],
+					'os.arch': os.arch(),
+					'os.platform': os.platform(),
+					'os.release': os.release()
+				}
+			}
+		});
+	}
+} catch (err) {
+	console.log('Could not initialize Sentry, no worries.');
+}
+
+const customConfig: Config = {
+	dictionaries: [adjectives, colors, animals],
+	style: 'capital',
+	separator: ' ',
+	length: 3
+};
+
+export const version = currentVersion;
+export const asyncExecShell = util.promisify(child.exec);
+export const asyncSleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+export const sentry = Sentry;
+
+export const uniqueName = () => uniqueNamesGenerator(customConfig);
+
+export const saveBuildLog = async ({ line, buildId, applicationId }) => {
+	const addTimestamp = `${generateTimestamp()} ${line}`;
+	return await buildLogQueue.add(buildId, { buildId, line: addTimestamp, applicationId });
+};
+
+export const isTeamIdTokenAvailable = (request) => {
+	const cookie = request.headers.cookie
+		?.split(';')
+		.map((s) => s.trim())
+		.find((s) => s.startsWith('teamId='))
+		?.split('=')[1];
+	if (!cookie) {
+		return getTeam(request);
+	} else {
+		return cookie;
+	}
+};
+
+export const getTeam = (event) => {
+	const cookies = Cookie.parse(event.request.headers.get('cookie'));
+	if (cookies.teamId) {
+		return cookies.teamId;
+	} else if (event.locals.session.data.teamId) {
+		return event.locals.session.data.teamId;
+	}
+	return null;
+};
+
+export const getUserDetails = async (event, isAdminRequired = true) => {
+	const teamId = getTeam(event);
+	const userId = event.locals.session.data.userId || null;
+	const { permission = 'read' } = await db.prisma.permission.findFirst({
+		where: { teamId, userId },
+		select: { permission: true },
+		rejectOnNotFound: true
+	});
+	const payload = {
+		teamId,
+		userId,
+		permission,
+		status: 200,
+		body: {
+			message: 'OK'
+		}
+	};
+	if (isAdminRequired && permission !== 'admin' && permission !== 'owner') {
+		payload.status = 401;
+		payload.body.message =
+			'You do not have permission to do this. \nAsk an admin to modify your permissions.';
+	}
+
+	return payload;
+};
+
+export function getEngine(engine) {
+	return engine === '/var/run/docker.sock' ? 'unix:///var/run/docker.sock' : engine;
+}
+
+// export async function saveSshKey(destination) {
+// 	return await asyncExecShell(
+// 		`echo '${destination.sshPrivateKey}' > /tmp/id_rsa_${destination.id} && chmod 600 /tmp/id_rsa_${destination.id}`
+// 	);
+// }
+export async function removeContainer(id, engine) {
+	const host = getEngine(engine);
+	try {
+		const { stdout } = await asyncExecShell(
+			`DOCKER_HOST=${host} docker inspect --format '{{json .State}}' ${id}`
+		);
+		if (JSON.parse(stdout).Running) {
+			await asyncExecShell(`DOCKER_HOST=${host} docker stop -t 0 ${id}`);
+			await asyncExecShell(`DOCKER_HOST=${host} docker rm ${id}`);
+		}
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+}
+
+export const removeDestinationDocker = async ({ id, engine }) => {
+	return await removeContainer(id, engine);
+};
+
+export const createDirectories = async ({ repository, buildId }) => {
+	const repodir = `/tmp/build-sources/${repository}/`;
+	const workdir = `/tmp/build-sources/${repository}/${buildId}`;
+
+	await asyncExecShell(`mkdir -p ${workdir}`);
+
+	return {
+		workdir,
+		repodir
+	};
+};
+
+export function generateTimestamp() {
+	return `${dayjs().format('HH:mm:ss.SSS')} `;
+}
+
+export function getDomain(domain) {
+	return domain?.replace('https://', '').replace('http://', '');
+}
---
+++ src/lib/database/applications.ts
@@ -0,0 +1,242 @@
+import { decrypt, encrypt } from '$lib/crypto';
+import { asyncExecShell, getEngine } from '$lib/common';
+
+import { getDomain, removeDestinationDocker } from '$lib/common';
+import { prisma } from './common';
+
+export async function listApplications(teamId) {
+	return await prisma.application.findMany({ where: { teams: { some: { id: teamId } } } });
+}
+
+export async function newApplication({ name, teamId }) {
+	return await prisma.application.create({
+		data: {
+			name,
+			teams: { connect: { id: teamId } },
+			settings: { create: { debug: false, previews: false } }
+		}
+	});
+}
+
+export async function importApplication({
+	name,
+	teamId,
+	fqdn,
+	port,
+	buildCommand,
+	startCommand,
+	installCommand
+}) {
+	return await prisma.application.create({
+		data: {
+			name,
+			fqdn,
+			port,
+			buildCommand,
+			startCommand,
+			installCommand,
+			teams: { connect: { id: teamId } }
+		}
+	});
+}
+
+export async function removeApplication({ id, teamId }) {
+	const { fqdn, destinationDockerId, destinationDocker } = await prisma.application.findUnique({
+		where: { id },
+		include: { destinationDocker: true }
+	});
+	const domain = getDomain(fqdn);
+	if (destinationDockerId) {
+		const host = getEngine(destinationDocker.engine);
+		const { stdout: containers } = await asyncExecShell(
+			`DOCKER_HOST=${host} docker ps -a --filter network=${destinationDocker.network} --filter name=${id} --format '{{json .}}'`
+		);
+		if (containers) {
+			const containersArray = containers.trim().split('\n');
+			for (const container of containersArray) {
+				const containerObj = JSON.parse(container);
+				const id = containerObj.ID;
+				const preview = containerObj.Image.split('-')[1];
+				await removeDestinationDocker({ id, engine: destinationDocker.engine });
+			}
+		}
+	}
+
+	await prisma.applicationSettings.deleteMany({ where: { application: { id } } });
+	await prisma.buildLog.deleteMany({ where: { applicationId: id } });
+	await prisma.secret.deleteMany({ where: { applicationId: id } });
+	await prisma.application.deleteMany({ where: { id, teams: { some: { id: teamId } } } });
+}
+
+export async function getApplicationWebhook({ projectId, branch }) {
+	try {
+		let applications = await prisma.application.findMany({
+			where: { projectId, branch },
+			include: {
+				destinationDocker: true,
+				settings: true,
+				gitSource: { include: { githubApp: true, gitlabApp: true } },
+				secrets: true
+			}
+		});
+		for (const application of applications) {
+			if (application.gitSource?.githubApp?.clientSecret) {
+				application.gitSource.githubApp.clientSecret = decrypt(
+					application.gitSource.githubApp.clientSecret
+				);
+			}
+			if (application.gitSource?.githubApp?.webhookSecret) {
+				application.gitSource.githubApp.webhookSecret = decrypt(
+					application.gitSource.githubApp.webhookSecret
+				);
+			}
+			if (application.gitSource?.githubApp?.privateKey) {
+				application.gitSource.githubApp.privateKey = decrypt(
+					application.gitSource.githubApp.privateKey
+				);
+			}
+			if (application?.gitSource?.gitlabApp?.appSecret) {
+				application.gitSource.gitlabApp.appSecret = decrypt(
+					application.gitSource.gitlabApp.appSecret
+				);
+			}
+			if (application?.gitSource?.gitlabApp?.webhookToken) {
+				application.gitSource.gitlabApp.webhookToken = decrypt(
+					application.gitSource.gitlabApp.webhookToken
+				);
+			}
+			if (application?.secrets.length > 0) {
+				application.secrets = application.secrets.map((s) => {
+					s.value = decrypt(s.value);
+					return s;
+				});
+			}
+		}
+		return [...applications];
+	} catch (e) {
+		throw { status: 404, body: { message: e.message } };
+	}
+}
+export async function getApplicationById({ id }) {
+	const body = await prisma.application.findFirst({
+		where: { id },
+		include: { destinationDocker: true }
+	});
+
+	return { ...body };
+}
+export async function getApplication({ id, teamId }) {
+	let body = await prisma.application.findFirst({
+		where: { id, teams: { some: { id: teamId } } },
+		include: {
+			destinationDocker: true,
+			settings: true,
+			gitSource: { include: { githubApp: true, gitlabApp: true } },
+			secrets: true
+		}
+	});
+
+	if (body?.gitSource?.githubApp?.clientSecret) {
+		body.gitSource.githubApp.clientSecret = decrypt(body.gitSource.githubApp.clientSecret);
+	}
+	if (body?.gitSource?.githubApp?.webhookSecret) {
+		body.gitSource.githubApp.webhookSecret = decrypt(body.gitSource.githubApp.webhookSecret);
+	}
+	if (body?.gitSource?.githubApp?.privateKey) {
+		body.gitSource.githubApp.privateKey = decrypt(body.gitSource.githubApp.privateKey);
+	}
+	if (body?.gitSource?.gitlabApp?.appSecret) {
+		body.gitSource.gitlabApp.appSecret = decrypt(body.gitSource.gitlabApp.appSecret);
+	}
+	if (body?.secrets.length > 0) {
+		body.secrets = body.secrets.map((s) => {
+			s.value = decrypt(s.value);
+			return s;
+		});
+	}
+
+	return { ...body };
+}
+
+export async function configureGitRepository({ id, repository, branch, projectId, webhookToken }) {
+	if (webhookToken) {
+		const encryptedWebhookToken = encrypt(webhookToken);
+		return await prisma.application.update({
+			where: { id },
+			data: {
+				repository,
+				branch,
+				projectId,
+				gitSource: { update: { gitlabApp: { update: { webhookToken: encryptedWebhookToken } } } }
+			}
+		});
+	} else {
+		return await prisma.application.update({
+			where: { id },
+			data: { repository, branch, projectId }
+		});
+	}
+}
+
+export async function configureBuildPack({ id, buildPack }) {
+	return await prisma.application.update({ where: { id }, data: { buildPack } });
+}
+
+export async function configureApplication({
+	id,
+	buildPack,
+	name,
+	fqdn,
+	port,
+	installCommand,
+	buildCommand,
+	startCommand,
+	baseDirectory,
+	publishDirectory
+}) {
+	return await prisma.application.update({
+		where: { id },
+		data: {
+			buildPack,
+			fqdn,
+			port,
+			installCommand,
+			buildCommand,
+			startCommand,
+			baseDirectory,
+			publishDirectory,
+			name
+		}
+	});
+}
+
+export async function setApplicationSettings({ id, debug, previews, dualCerts }) {
+	return await prisma.application.update({
+		where: { id },
+		data: { settings: { update: { debug, previews, dualCerts } } },
+		include: { destinationDocker: true }
+	});
+}
+
+export async function createBuild({
+	id,
+	applicationId,
+	destinationDockerId,
+	gitSourceId,
+	githubAppId,
+	gitlabAppId,
+	type
+}) {
+	return await prisma.build.create({
+		data: {
+			id,
+			applicationId,
+			destinationDockerId,
+			gitSourceId,
+			githubAppId,
+			gitlabAppId,
+			status: 'running',
+			type
+		}
+	});
+}
