00c959e618392dc17769e73e3b9c66711c9fc592
zero-0002 <zero.dev0002@gmail.com>
2022-04-02 23:25:55 +0200

    feat: Add abilitry to paste env files

---
+++ src/routes/applications/[id]/logs/index.svelte
@@ -0,0 +1,119 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+	import { onDestroy, onMount } from 'svelte';
+	export const load: Load = async ({ fetch, params, url, stuff }) => {
+		let endpoint = `/applications/${params.id}/logs.json`;
+		const res = await fetch(endpoint);
+		if (res.ok) {
+			return {
+				props: {
+					application: stuff.application,
+					...(await res.json())
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let application;
+	import { page } from '$app/stores';
+	import LoadingLogs from './_Loading.svelte';
+	import { getDomain } from '$lib/components/common';
+	import { get } from '$lib/api';
+	import { errorNotification } from '$lib/form';
+
+	let loadLogsInterval = null;
+	let logs = [];
+	let followingBuild;
+	let followingInterval;
+	let logsEl;
+
+	const { id } = $page.params;
+	onMount(async () => {
+		loadLogs();
+		loadLogsInterval = setInterval(() => {
+			loadLogs();
+		}, 1000);
+	});
+	onDestroy(() => {
+		clearInterval(loadLogsInterval);
+		clearInterval(followingInterval);
+	});
+	async function loadLogs() {
+		try {
+			const newLogs = await get(`/applications/${id}/logs.json`);
+			logs = newLogs.logs;
+			return;
+		} catch ({ error }) {
+			return errorNotification(error);
+		}
+	}
+
+	function followBuild() {
+		followingBuild = !followingBuild;
+		if (followingBuild) {
+			followingInterval = setInterval(() => {
+				logsEl.scrollTop = logsEl.scrollHeight;
+				window.scrollTo(0, document.body.scrollHeight);
+			}, 100);
+		} else {
+			window.clearInterval(followingInterval);
+		}
+	}
+</script>
+
+<div class="flex space-x-1 p-6 font-bold">
+	<div class="mr-4 text-2xl tracking-tight">
+		Application logs of <a href={application.fqdn} target="_blank">{getDomain(application.fqdn)}</a>
+	</div>
+</div>
+<div class="flex flex-row justify-center space-x-2 px-10 pt-6">
+	{#if logs.length === 0}
+		<div class="text-xl font-bold tracking-tighter">Waiting for the logs...</div>
+	{:else}
+		<div class="relative w-full">
+			<LoadingLogs />
+			<div class="flex justify-end sticky top-0 p-2">
+				<button
+					on:click={followBuild}
+					class="bg-transparent"
+					data-tooltip="Follow logs"
+					class:text-green-500={followingBuild}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-6 h-6"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<circle cx="12" cy="12" r="9" />
+						<line x1="8" y1="12" x2="12" y2="16" />
+						<line x1="12" y1="8" x2="12" y2="16" />
+						<line x1="16" y1="12" x2="12" y2="16" />
+					</svg>
+				</button>
+			</div>
+			<div
+				class="font-mono w-full leading-6 text-left text-md tracking-tighter rounded bg-coolgray-200 py-5 px-6 whitespace-pre-wrap break-words overflow-auto max-h-[80vh] -mt-12 overflow-y-scroll scrollbar-w-1 scrollbar-thumb-coollabs scrollbar-track-coolgray-200"
+				bind:this={logsEl}
+			>
+				<div class="px-2">
+					{#each logs as log}
+						{log + '\n'}
+					{/each}
+				</div>
+			</div>
+		</div>
+	{/if}
+</div>
---
+++ src/routes/applications/[id]/secrets/index.svelte
@@ -0,0 +1,74 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+	export const load: Load = async ({ fetch, params, stuff }) => {
+		let endpoint = `/applications/${params.id}/secrets.json`;
+		const res = await fetch(endpoint);
+		if (res.ok) {
+			return {
+				props: {
+					application: stuff.application,
+					...(await res.json())
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${endpoint}`)
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let secrets;
+	export let application;
+	import Secret from './_Secret.svelte';
+	import { getDomain } from '$lib/components/common';
+	import { page } from '$app/stores';
+	import { get } from '$lib/api';
+	import BatchSecrets from './_BatchSecrets.svelte';
+
+	const { id } = $page.params;
+
+	async function refreshSecrets() {
+		const data = await get(`/applications/${id}/secrets.json`);
+		secrets = [...data.secrets];
+	}
+</script>
+
+<div class="flex space-x-1 p-6 font-bold">
+	<div class="mr-4 text-2xl tracking-tight">
+		Secrets for <a href={application.fqdn} target="_blank">{getDomain(application.fqdn)}</a>
+	</div>
+</div>
+<div class="mx-auto max-w-6xl rounded-xl px-6 pt-4">
+	<table class="mx-auto border-separate text-left">
+		<thead>
+			<tr class="h-12">
+				<th scope="col">Name</th>
+				<th scope="col">Value</th>
+				<th scope="col" class="w-64 text-center">Need during buildtime?</th>
+				<th scope="col" class="w-96 text-center">Action</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each secrets as secret}
+				{#key secret.id}
+					<tr>
+						<Secret
+							name={secret.name}
+							value={secret.value}
+							isBuildSecret={secret.isBuildSecret}
+							on:refresh={refreshSecrets}
+						/>
+					</tr>
+				{/key}
+			{/each}
+			<tr>
+				<Secret isNewSecret on:refresh={refreshSecrets} />
+			</tr>
+		</tbody>
+	</table>
+
+	<BatchSecrets {secrets} {id} {refreshSecrets} />
+</div>
