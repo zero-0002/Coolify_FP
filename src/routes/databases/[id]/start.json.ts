2317d8fde340f9636c59746a80f399e85bfa4bb9
zero-0002 <zero.dev0002@gmail.com>
2022-04-05 01:13:25 +0200

    Implemented typing for dockerfile configuration

--- src/routes/databases/[id]/start.json.ts
+++ src/routes/databases/[id]/start.json.ts
@@ -1,10 +1,11 @@
 import { asyncExecShell, createDirectories, getEngine, getUserDetails } from '$lib/common';
 import * as db from '$lib/database';
+import { generateDatabaseConfiguration, ErrorHandler } from '$lib/database';
 import { promises as fs } from 'fs';
 import yaml from 'js-yaml';
 import type { RequestHandler } from '@sveltejs/kit';
-import { ErrorHandler, getServiceImage } from '$lib/database';
-import { makeLabelForServices } from '$lib/buildPacks/common';
+import { makeLabelForStandaloneDatabase } from '$lib/buildPacks/common';
+import { startTcpProxy } from '$lib/haproxy';
 import type { ComposeFile } from '$lib/types/composeFile';
 
 export const post: RequestHandler = async (event) => {
@@ -14,56 +15,67 @@ export const post: RequestHandler = async (event) => {
 	const { id } = event.params;
 
 	try {
-		const service = await db.getService({ id, teamId });
-		const { type, version, destinationDockerId, destinationDocker, serviceSecret } = service;
+		const database = await db.getDatabase({ id, teamId });
+		const {
+			type,
+			destinationDockerId,
+			destinationDocker,
+			publicPort,
+			settings: { isPublic }
+		} = database;
+		const { privatePort, environmentVariables, image, volume, ulimits } =
+			generateDatabaseConfiguration(database);
+
 		const network = destinationDockerId && destinationDocker.network;
 		const host = getEngine(destinationDocker.engine);
+		const engine = destinationDocker.engine;
+		const volumeName = volume.split(':')[0];
+		const labels = await makeLabelForStandaloneDatabase({ id, image, volume });
 
 		const { workdir } = await createDirectories({ repository: type, buildId: id });
-		const image = getServiceImage(type);
 
-		const config = {
-			image: `${image}:${version}`,
-			environmentVariables: {}
-		};
-		if (serviceSecret.length > 0) {
-			serviceSecret.forEach((secret) => {
-				config.environmentVariables[secret.name] = secret.value;
-			});
-		}
 		const composeFile: ComposeFile = {
 			version: '3.8',
 			services: {
 				[id]: {
 					container_name: id,
-					image: config.image,
+					image,
 					networks: [network],
-					environment: config.environmentVariables,
-					restart: 'always',
-					labels: makeLabelForServices('nocodb')
+					environment: environmentVariables,
+					volumes: [volume],
+					ulimits,
+					labels,
+					restart: 'always'
 				}
 			},
 			networks: {
 				[network]: {
 					external: true
 				}
+			},
+			volumes: {
+				[volumeName]: {
+					external: true
+				}
 			}
 		};
 		const composeFileDestination = `${workdir}/docker-compose.yaml`;
 		await fs.writeFile(composeFileDestination, yaml.dump(composeFile));
-
 		try {
-			if (version === 'latest') {
-				await asyncExecShell(
-					`DOCKER_HOST=${host} docker compose -f ${composeFileDestination} pull`
-				);
-			}
+			await asyncExecShell(`DOCKER_HOST=${host} docker volume create ${volumeName}`);
+		} catch (error) {
+			console.log(error);
+		}
+		try {
 			await asyncExecShell(`DOCKER_HOST=${host} docker compose -f ${composeFileDestination} up -d`);
+			if (isPublic) await startTcpProxy(destinationDocker, id, publicPort, privatePort);
 			return {
 				status: 200
 			};
 		} catch (error) {
-			return ErrorHandler(error);
+			throw {
+				error
+			};
 		}
 	} catch (error) {
 		return ErrorHandler(error);
