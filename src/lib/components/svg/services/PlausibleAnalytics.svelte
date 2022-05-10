663f0806d8a385b72a6635bb080a2eac814fa8bc
zero-0002 <zero.dev0002@gmail.com>
2022-05-10 10:12:13 +0200

    feat: WP could have custom db

---
+++ src/lib/components/svg/services/PlausibleAnalytics.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let isAbsolute = false;
+</script>
+
+<img
+	alt="plausible logo"
+	class={isAbsolute ? 'w-9 absolute top-0 left-0 -m-4' : 'w-6 mx-auto'}
+	src="/plausible.png"
+/>
