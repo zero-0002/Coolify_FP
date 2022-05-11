06da9fd9c8651a56a99819cf70073a40dfc46711
zero-0002 <zero.dev0002@gmail.com>
2022-05-11 11:02:21 +0200

    fix: Service checks

--- src/routes/services/[id]/check.json.ts
+++ src/routes/services/[id]/check.json.ts
@@ -1,44 +1,20 @@
-import { dev } from '$app/env';
-import { checkDomainsIsValidInDNS, getDomain, getUserDetails, isDNSValid } from '$lib/common';
+import { asyncExecShell, getDomain, getEngine, getUserDetails } from '$lib/common';
 import * as db from '$lib/database';
 import { ErrorHandler } from '$lib/database';
-import type { RequestHandler } from '@sveltejs/kit';
-import { promises as dns } from 'dns';
-import getPort from 'get-port';
 import { t } from '$lib/translations';
-
-export const get: RequestHandler = async (event) => {
-	const { status, body } = await getUserDetails(event);
-	if (status === 401) return { status, body };
-	const domain = event.url.searchParams.get('domain');
-	if (!domain) {
-		return {
-			status: 500,
-			body: {
-				message: t.get('application.domain_required')
-			}
-		};
-	}
-	try {
-		await isDNSValid(event, domain);
-		return {
-			status: 200
-		};
-	} catch (error) {
-		return ErrorHandler(error);
-	}
-};
+import type { RequestHandler } from '@sveltejs/kit';
+import getPort from 'get-port';
 
 export const post: RequestHandler = async (event) => {
 	const { status, body } = await getUserDetails(event);
 	if (status === 401) return { status, body };
 
 	const { id } = event.params;
-	let { exposePort, fqdn, forceSave, dualCerts } = await event.request.json();
-	fqdn = fqdn.toLowerCase();
+	let { fqdn, exposePort } = await event.request.json();
+
+	if (fqdn) fqdn = fqdn.toLowerCase();
 
 	try {
-		const { isDNSCheckEnabled } = await db.prisma.setting.findFirst({});
 		const found = await db.isDomainConfigured({ id, fqdn });
 		if (found) {
 			throw {
@@ -47,7 +23,6 @@ export const post: RequestHandler = async (event) => {
 				})
 			};
 		}
-
 		if (exposePort) {
 			exposePort = Number(exposePort);
 
@@ -60,13 +35,11 @@ export const post: RequestHandler = async (event) => {
 				throw { message: `Port ${exposePort} is already in use.` };
 			}
 		}
-
-		if (isDNSCheckEnabled && !dev && !forceSave) {
-			return await checkDomainsIsValidInDNS({ event, fqdn, dualCerts });
-		}
-
 		return {
-			status: 200
+			status: found ? 500 : 200,
+			body: {
+				error: found && `Domain ${getDomain(fqdn).replace('www.', '')} is already used.`
+			}
 		};
 	} catch (error) {
 		return ErrorHandler(error);
