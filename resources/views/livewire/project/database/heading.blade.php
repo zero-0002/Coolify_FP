474d49c8e166edfe0f0e5bd90b94d98260da219b
zero-0002 <zero.dev0002@gmail.com>
2025-06-24 13:37:05 +0200

    fix(ui): update confirmation button text for stopping database and service

--- resources/views/livewire/project/database/heading.blade.php
+++ resources/views/livewire/project/database/heading.blade.php
@@ -60,7 +60,7 @@
                             'If the database is currently in use data could be lost.',
                             'All non-persistent data of this database (containers, networks, unused images) will be deleted (don\'t worry, no data is lost and you can start the database again).',
                         ]" :confirmWithText="false" :confirmWithPassword="false"
-                        step1ButtonText="Continue" step2ButtonText="Stop Database">
+                        step1ButtonText="Continue" step2ButtonText="Confirm">
                         <x-slot:button-title>
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-error" viewBox="0 0 24 24"
                                 stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
