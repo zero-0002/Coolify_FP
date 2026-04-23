ac820700ce6af9cbbee362069f05637e8b0a704c
zero-0002 <zero.dev0002@gmail.com>
2026-04-23 16:13:09 +0530

    feat(destination): show resources that are deployed on the destination

--- resources/views/livewire/destination/show.blade.php
+++ resources/views/livewire/destination/show.blade.php
@@ -28,4 +28,24 @@
             @endif
         </div>
     </form>
+
+    @if ($destination->getMorphClass() === 'App\Models\StandaloneDocker')
+        <div class="pt-6">
+            <h3>Resources</h3>
+            <div class="pb-2 text-xs opacity-70">Applications, services, and databases deployed to this network.</div>
+            @if (count($resources) === 0)
+                <div class="text-xs opacity-70 pt-2">No resources are using this destination.</div>
+            @else
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-2 pt-2">
+                    @foreach ($resources as $row)
+                        <a href="{{ $row['url'] }}"
+                            class="relative flex flex-col dark:text-white coolbox group cursor-pointer">
+                            <div class="box-title">{{ ucfirst($row['type']) }}: {{ $row['name'] }}</div>
+                            <div class="box-description">{{ $row['project'] }} / {{ $row['environment'] }}</div>
+                        </a>
+                    @endforeach
+                </div>
+            @endif
+        </div>
+    @endif
 </div>
